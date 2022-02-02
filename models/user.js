0e4bfa190197fe190b548ba22cbbb26719d8aaab
boury_a <boury_a@etna-alternance.net>
2022-02-02 17:16:26 +0100

    added justification

--- models/user.js
+++ models/user.js
@@ -1,14 +1,38 @@
 const db = require("../settings/database");
-const {DataTypes} = require('sequelize')
+const { DataTypes } = require('sequelize')
 const roles = ["buyer", "seller", "admin"];
 
 const User = db.define('Users', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true // Automatically gets converted to SERIAL for postgres
+        autoIncrement: true
     },
     email: {
         type: DataTypes.STRING, unique: true
+    },
+    firstName: {
+        type: DataTypes.STRING(50)
+    },
+    lastName: {
+        type: DataTypes.STRING(50)
+    },
+    password: {
+        type: DataTypes.STRING
+    },
+    birthDate: {
+        type: DataTypes.DATEONLY
+    },
+    firstName: {
+        type: DataTypes.STRING(50)
+    },
+    role: {
+        type: DataTypes.ENUM(roles)
+    },
+    createdAt: {
+        type: DataTypes.DATE
+    },
+    modifiedAt: {
+        type: DataTypes.DATE
     }
 })
