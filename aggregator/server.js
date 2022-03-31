6fe4bf84d8ca88e09c242c2aad5253f7ce5f84bf
boury_a <boury_a@etna-alternance.net>
2022-03-31 17:32:13 +0200

    fix

--- aggregator/server.js
+++ aggregator/server.js
@@ -2,6 +2,6 @@ const app = require("./app")
 
 //NETWORK SETTINGS
 app.listen(process.env.APP_AGGREGATOR_PORT, () => {
-    console.log(`Backend is running on port ${process.env.APP_AGGREGATOR_PORT}`);
+    console.log(`Backend is running on port: ${process.env.APP_AGGREGATOR_PORT}`);
 });
 
